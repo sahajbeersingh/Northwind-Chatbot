754876b7e3e568daf8f5991b40c4f2c64df6ab08
FIXED-TERM Singh Sahajbeer (BD/PTD-TAF) <gee7kor@bosch.com>
2026-06-29 16:12:35 +0530

    debugging

--- llm/model.py
+++ llm/model.py
@@ -3,42 +3,31 @@ import requests
 import streamlit as st
 from dotenv import load_dotenv
 print("Importing llm.model")
-# def ask_model(message):
-#     load_dotenv()
-#     try:
-#         api_key = st.secrets["MODEL_FARM_API_KEY"]
-#         url = st.secrets["URL"]
-#         model_name = st.secrets["MODEL"]
-#     except Exception:
-#         api_key = os.getenv("MODEL_FARM_API_KEY")
-#         url = os.getenv("URL")
-#         model_name = os.getenv("MODEL")
-#     print("Calling ask_model")
-#     response = requests.post(
-#         url,
-#         headers={
-#             "Authorization": f"Bearer {api_key}",
-#             "Content-Type": "application/json"
-#             },
-#             json={
-#                 "model": model_name,
-#                 "messages": message
-#                 }
-#         )
-#     print("Status:", response.status_code)
-#     print(response.text[:500])
-#     return response.json()
-
 def ask_model(message):
-    return {
-        "choices": [
-            {
-                "message": {
-                    "content": '{"follow_previous": false, "intent": "OUT_OF_SCOPE", "entity": "", "message": "Test"}'
+    load_dotenv()
+    try:
+        api_key = st.secrets["MODEL_FARM_API_KEY"]
+        url = st.secrets["URL"]
+        model_name = st.secrets["MODEL"]
+    except Exception:
+        api_key = os.getenv("MODEL_FARM_API_KEY")
+        url = os.getenv("URL")
+        model_name = os.getenv("MODEL")
+    print("Calling ask_model")
+    response = requests.post(
+        url,
+        headers={
+            "Authorization": f"Bearer {api_key}",
+            "Content-Type": "application/json"
+            },
+            json={
+                "model": model_name,
+                "messages": message
                 }
-            }
-        ]
-    }
+        )
+    print("Status:", response.status_code)
+    print(response.text[:500])
+    return response.json()
 if __name__=="__main__":
     print(ask_model([{
         "role":"system",
--- metadata/schema_manager.py
+++ metadata/schema_manager.py
@@ -2,13 +2,13 @@ import requests
 import os
 import streamlit as st
 from dotenv import load_dotenv
-load_dotenv()
-try:
-    Base_url = st.secrets["BASE_URL"]
-except Exception:
-    Base_url = os.getenv("BASE_URL")
-METADATA_URL = Base_url+"/$metadata"
 def refresh_metadata():
+    load_dotenv()
+    try:
+        Base_url = st.secrets["BASE_URL"]
+    except Exception:
+        Base_url = os.getenv("BASE_URL")
+    METADATA_URL = Base_url+"/$metadata"
     response = requests.get(METADATA_URL)
     response.raise_for_status()
     with open("metadata/metadata.xml", "wb") as f:
